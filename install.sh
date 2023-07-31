ed60c55dbb8ca597a37799881e487ddcff906186
Azis W Katamsi <astrojim.blankon@gmail.com>
2023-07-31 18:52:55 +0700

    Summery

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 APP_PATH="/usr/local/bin/"
-ICONS_PATH="WLAN"
+ICONS_PATH="WLAN/Icons"
 WIFI_PATH="WifiStatusBar"
-RES_PATH="/Library/Application Support/"
+RES_PATH="/Library/Application Support/WLAN"
 START="/Library/Application Support/WLAN/StatusBarApp.app"
 
-sudo mkdir "$RES_PATH"WLAN
+#sudo mkdir "$RES_PATH"
 sudo cp -rf "$ICONS_PATH" "$RES_PATH"
 sudo cp -rf "$WIFI_PATH" "$APP_PATH"
 sleep 5
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pkill -a StatusBar
-WLAN="/Library/Application Support/WLAN"
+#pkill -a StatusBar
+WLAN="/Library/Application Support/WLAN/Icons"
 BIN="/usr/local/bin/WifiStatusBar"
 sudo rm -rf "$WLAN"
 sudo rm -rf "$BIN"
+
 exit
